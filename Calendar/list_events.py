7d7b5e5024ee8dc9422236d9da5b06ce1bb1e427
siddharth-patel-23 <siddharth.ext@grofers.com>
2021-03-07 15:36:44 +0530

    Added Create Event Method

--- Calendar/list_events.py
+++ Calendar/list_events.py
@@ -4,9 +4,7 @@ import os
 from datetime import datetime
 
 
-def get_events(event_id):
-    current_path = os.path.dirname('client_secret.json')
-    print("PATHH ", current_path)
+def get_events(calendarId):
     CLIENT_SECRET_FILE = "Calendar/client_secret.json"
     API_NAME = 'calendar'
     API_VERSION = 'v3'
@@ -15,8 +13,8 @@ def get_events(event_id):
     service = Google.Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
     page_token = None
     while True:
-        events = service.events().list(calendarId='primary', pageToken=page_token,
-                                       timeMin=Google.convert_to_RFC_datetime(2021, 3, 6, 0, 0),
-                                       timeMax=Google.convert_to_RFC_datetime(2021, 3, 6, 23, 59)).execute()
-        pprint(events)
+        events = service.events().list(calendarId=calendarId, pageToken=page_token,
+                                       timeMin=Google.convert_to_RFC_datetime(2021, 3, 3, 0, 0),
+                                       timeMax=Google.convert_to_RFC_datetime(2021, 3, 3, 23, 59)).execute()
+        # pprint(events)
         return events['items']  # Returns events of the day
